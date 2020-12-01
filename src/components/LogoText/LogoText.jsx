2ee6fa388aeca0dd2b00477ebbd41ca21986fb7e
Alan Eicker <alaneicker@gmail.com>
2020-11-30 21:59:31 -0600

    Updates Header and LogoText font sizes

--- src/components/LogoText/LogoText.jsx
+++ src/components/LogoText/LogoText.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const LogoText = ({ mobileLogoText, logoText, strapline }) => (
-  <div className="text-size-20">
+  <div>
     <span className="display-none display-inline@medium">
-      <span className="text-weight-semibold">{logoText}</span>
+      <span className="text-weight-semibold text-size-20">{logoText}</span>
       <span className="display-none display-inline@large">
         <span className="margin-left-8 margin-right-8">&bull;</span>
-        <span className="text-weight-light">{strapline}</span>
+        <span className="text-weight-light text-size-20">{strapline}</span>
       </span>
     </span>
-    <span className="display-none@medium">{mobileLogoText}</span>
+    <span className="display-none@medium text-size-24">{mobileLogoText}</span>
   </div>
 );
 
