61b3a06305eee5dfce6ec0c1febbd59f25d6b95d
Alan Eicker <alaneicker@gmail.com>
2020-12-02 12:28:15 -0600

    Service worker relocated to index.html

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './App';
 import './styles/main.scss';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-}
-
 (async () => {
   if (process.env.NODE_ENV !== 'production') {
     const axe = await import('react-axe');
